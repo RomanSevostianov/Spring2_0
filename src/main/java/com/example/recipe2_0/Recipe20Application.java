41d63c71791ded3bed36e792df44a07e77bd283b
Роман <pangujailbreake58@gmail.com>
2023-03-15 12:03:35 +0300

    1

---
+++ src/main/java/com/example/recipe2_0/Recipe20Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe2_0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recipe20Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Recipe20Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/recipe2_0/Recipe20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipe2_0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recipe20ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
